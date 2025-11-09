6a63d07a727498c96d10a88d880ff8f2250e9716
Mazen-Alzahrani <maznhmz123@gmail.com>
2025-11-09 14:48:47 +0300

    تغييرات بسيطه في السلايدر

--- assets/script.js
+++ assets/script.js
@@ -26,64 +26,59 @@ document.addEventListener('click', (e) => {
   }
 });
 
-// Carousel Slider JavaScript - Original Version
+// Carousel Slider JavaScript
 let nextDom = document.getElementById('next');
 let prevDom = document.getElementById('prev');
 
 let carouselDom = document.querySelector('.carousel');
 if (carouselDom) {
-  let SliderDom = carouselDom.querySelector('.carousel .list');
-  let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
-  let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
-  let timeDom = document.querySelector('.carousel .time');
+    let SliderDom = carouselDom.querySelector('.carousel .list');
+    let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+    let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+    let timeDom = document.querySelector('.carousel .time');
 
-  thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-  let timeRunning = 500;
- // let timeAutoNext = 27000;
+    thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+    let timeRunning = 3000;
+    let timeAutoNext = 15000;
 
-  if (nextDom) {
-    nextDom.onclick = function() {
-      showSlider('next');
-    };
-  }
-
-  if (prevDom) {
-    prevDom.onclick = function() {
-      showSlider('prev');
-    };
-  }
-
-  let runTimeOut;
-  /*let runNextAuto = setTimeout(() => {
-    if (nextDom) nextDom.click();
-  }, timeAutoNext);*/
-
-  function showSlider(type) {
-    let SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
-    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
-    
-    if (type === 'next') {
-      SliderDom.appendChild(SliderItemsDom[0]);
-      thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-      carouselDom.classList.add('next');
-    } else {
-      SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
-      thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-      carouselDom.classList.add('prev');
+    nextDom.onclick = function(){
+        showSlider('next');    
     }
-    
-    clearTimeout(runTimeOut);
-    runTimeOut = setTimeout(() => {
-      carouselDom.classList.remove('next');
-      carouselDom.classList.remove('prev');
-    }, timeRunning);
 
-    // Auto-play is disabled - uncomment below to enable
-    // clearTimeout(runNextAuto);
-    // runNextAuto = setTimeout(() => {
-    //   if (nextDom) nextDom.click();
-    // }, timeAutoNext);
-  }
+    prevDom.onclick = function(){
+        showSlider('prev');    
+    }
+
+    let runTimeOut;
+    let runNextAuto = setTimeout(() => {
+        nextDom.click();
+    }, timeAutoNext);
+
+    function showSlider(type){
+        let SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+        let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+        
+        if(type === 'next'){
+            SliderDom.appendChild(SliderItemsDom[0]);
+            thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+            carouselDom.classList.add('next');
+        }else{
+            SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+            thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+            carouselDom.classList.add('prev');
+        }
+        
+        clearTimeout(runTimeOut);
+        runTimeOut = setTimeout(() => {
+            carouselDom.classList.remove('next');
+            carouselDom.classList.remove('prev');
+        }, timeRunning);
+
+        clearTimeout(runNextAuto);
+        runNextAuto = setTimeout(() => {
+            nextDom.click();
+        }, timeAutoNext);
+    }
 }
 
 
