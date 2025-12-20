00e14369e07a470eaf82e48807848a3f2847afdb
Mazen-Alzahrani <maznhmz123@gmail.com>
2025-12-20 21:10:53 +0300

    تعديلات

--- assets/script.js
+++ assets/script.js
@@ -38,8 +38,7 @@ if (carouselDom) {
     let timeDom = document.querySelector('.carousel .time');
 
     thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-    let timeRunning = 3000;
-    let timeAutoNext = 15000;
+    let timeRunning = 2250;
 
     nextDom.onclick = function(){
         showSlider('next');    
